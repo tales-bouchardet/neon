77489855b227f2afa10dc583ae8f37e10d7f73d4
tales-bouchardet <tales.bouchardet@danthe.dev.br>
2025-08-28 14:20:09 -0300

    Delete neon-web directory

--- neon-web/build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    #[cfg(windows)]
-    {
-        let mut res = winres::WindowsResource::new();
-        // Use seu caminho real do ícone:
-        res.set_icon("assets/icon.ico");
-        res.compile().expect("Falha ao compilar recursos");
-    }
-}
